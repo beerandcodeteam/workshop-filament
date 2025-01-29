40d41010300e837ff31159c1a9e94c1aa99a267d
Lucas Souza <lucasrsouzasilva@gmail.com>
2025-01-29 18:58:18 -0300

    fix

--- database/migrations/2025_01_29_123407_add_role_to_users_table.php
+++ database/migrations/2025_01_29_123407_add_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->after('id')->constrained('roles');
+            $table->foreignId('role_id')->after('id')->default(2)->constrained('roles');
         });
     }
 
