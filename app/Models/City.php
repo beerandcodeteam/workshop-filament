b8f7dbbe0e344b8fe9d6f64afeaf19699c367601
Lucas Souza <lucasrsouzasilva@gmail.com>
2025-01-29 13:38:02 -0300

    add world moldes

---
+++ app/Models/City.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class City extends Model
+{
+    protected $fillable = [
+        'name',
+        'state_id',
+        'state_code'
+    ];
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+}
